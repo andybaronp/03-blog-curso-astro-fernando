0553659d3b111dac32c0b6f762e7bcb655c7de12
Jhoandris B <jrbaronp@gmail.com>
2024-10-07 06:53:37 -0500

    rss config

--- src/content/blog/post-01.md
+++ src/content/blog/post-01.md
@@ -8,9 +8,9 @@ tags: [JavaScript, ES6, Programming]
 layout: ../../layouts/BlogLayout.astro
 ---
 
-# {frontmatter.title}
+# Explorando Funciones de ES6
 
-{/* Mostrar imagen */}
+{/_Mostrar imagen_/}
 
 JavaScript ES6 introdujo varias funciones nuevas que han hecho que la codificación en JavaScript sea más eficiente y agradable. Vamos a explorar algunas de estas funciones.
 
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,26 @@
+import rss from '@astrojs/rss'
+import type { APIRoute } from 'astro'
+import { getCollection } from 'astro:content'
+export const GET: APIRoute = async ({ params, request, site }) => {
+  const blogposts = await getCollection('blog')
+  return rss({
+    stylesheet: '/styles/styles.xsl',
+    // `<title>` field in output xml
+    title: 'Buzzwords Blog',
+    // `<description>` field in output xml
+    description: ' Blog y RSS',
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: site ?? '',
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: blogposts.map(({ data, slug }) => ({
+      title: data.title,
+      pubDate: data.date,
+      description: data.description,
+      link: `/posts/${slug}`,
+    })),
+    // (optional) inject custom xml
+    customData: `<language>es-mx</language>`,
+  })
+}
